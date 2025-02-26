9c4aae6a2d0fca23c59cba1ac2557f556a570d13
ajc32199 <adam.chiarella@gmail.com>
2025-02-26 15:57:11 -0600

    more styling

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/experience" element={<Experience />}></Route>
         </Routes>
       </main>
     </div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,11 +4,11 @@ function About() {
   return (
     <>
     <div>
-      <h1>About Me!</h1>
+      <h1 className='header-1'>About Me!</h1>
     </div>
     <div>
-      <p1>My name is Adam Chiarella, and Im a fourth year Computer Science Student studying at UMN - Duluth, graduation in May 2025</p1>
-      <p2>
+      <p1>My name is Adam Chiarella, and Im a fourth year Computer Science Student studying at UMN - Duluth, graduation in May 2025.</p1>
+      <p>
         I spent four years in the US Marine Corps, where I served with 1st Bn, 4th Marines in San Diego, CA. There, I had the honor
         of serving as a Mortars squad leader, supervising and directing the personal, professional, and tactical development of my Marines.
         I also served a secondary role as a Defense Travel systems respresentative, where I managed DTS claims from Marines in my company,
@@ -16,7 +16,7 @@ function About() {
 
         After my time in service, I went to study at UMN Duluth. There, I was a member of both Bulldog Rocketry, working with the Avionics and 
         and Technology Committee. I also was a member of Tau Kappa Epsilon - Upsilon Gamma Chapter, serving as president.
-      </p2>
+      </p>
     </div>
     </>
   )
