b895eb2d9737207d5446b0d7b9ab7ceb3ec20bf7
ajc32199 <adam.chiarella@gmail.com>
2025-02-26 18:47:36 -0600

    installed react bootstrap, updated navbar to use react bootstrap components

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,52 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../css/NavBar.css'
-
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 function NavBar() {
-  return (
-    <div className="navbar">
-        <div className="navbar-brand">
-            <Link to="/">Adams Website</Link>
-        </div>
-        <div>
-            <Link to="/" className='nav-link'>Home </Link>
-            <Link to="/About" className='nav-link'> About</Link>
-            <Link to="/Projects" className='nav-link'> Projects</Link>
-            <Link to="/Experience" className='nav-link'> Experience</Link>
-            <Link to="/Contact" className='nav-link'> Contact</Link>
-        </div>
-    </div>
+  
+  return(
+    <Navbar expand="lg" className="navbar">
+      <Container>
+        <Navbar.Brand href="/">Adams Website</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/About">About</Nav.Link>
+          <Nav.Link href="/Projects">Projects</Nav.Link>
+          <Nav.Link href="/Experience">Experience</Nav.Link>
+          <Nav.Link href="/Contact">Contact</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   )
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // return (
+  //   <div className="navbar">
+  //       <div className="navbar-brand">
+  //           <Link to="/">Adams Website</Link>
+  //       </div>
+  //       <div>
+  //           <Link to="/" className='nav-link'>Home </Link>
+  //           <Link to="/About" className='nav-link'> About</Link>
+  //           <Link to="/Projects" className='nav-link'> Projects</Link>
+  //           <Link to="/Experience" className='nav-link'> Experience</Link>
+  //           <Link to="/Contact" className='nav-link'> Contact</Link>
+  //       </div>
+  //   </div>
+  //)
 }
  
 export default NavBar
